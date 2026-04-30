bfcc7f58dd6fe6ce5e79568044f55cefa12222b5
Sanika Patil <info@luneva.co.in>
2026-04-30 15:57:55 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://luneva.co.in", "http://localhost:5173"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -29,5 +33,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  
 });
